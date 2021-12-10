sobek1886/Fork_SignCLIP

commit a63be995f80cf9f43fb3b34202251c5189f36667

Fix logits padding mask for wav2vec asr model (#2469)
Summary:
The logits were not correctly masked leading to significant WER
degradation for batched inference with padded inputs. This resolves it.
However there is minor degradation still from somewhere else.

# Before submitting

- [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
- [x] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/main/CONTRIBUTING.md)?
- [x] Did you make sure to update the docs?
- [ ] Did you write any new necessary tests?

## What does this PR do?
Fixes logits padding mask for wav2vec asr model.

## PR review
Anyone in the community is free to review the PR once the tests have passed.
If we didn't discuss your PR in Github issues there's a high chance it will not be merged.

## Did you have fun?
Make sure you had fun coding �

Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/2469

Reviewed By: arbabu123

Differential Revision: D32469676

Pulled By: alexeib

fbshipit-source-id: 154bb6523f9300e4b6c1cf2631424fd22f582bc5

fairseq/models/wav2vec/wav2vec2_asr.py
@@ -187,8 +187,10 @@ class Wav2VecCtc(BaseFairseqModel):
                 raise Exception(f"invalid blank mode {self.blank_mode}")
 
         if net_output["padding_mask"] is not None and net_output["padding_mask"].any():
-            logits[net_output["padding_mask"].T][..., 0] = float("inf")
-            logits[net_output["padding_mask"].T][..., 1:] = float("-inf")
+            number_of_classes = logits.size(-1)
+            masking_tensor = torch.ones(number_of_classes) * float("-inf")
+            masking_tensor[0] = float("inf")
+            logits[net_output["padding_mask"].T] = masking_tensor.type_as(logits)
 
         if normalize:
             logits = utils.log_softmax(logits.float(), dim=-1)

commit b7457b45e457627aded3996dceafd54f4a31a047

Fix logits masking bug introduced in #2469 (#2774)
Summary:
- Masking logits with float(inf) significantly degrades deconding
performance with language model and flashlight decoder. This fixes it.

# Before submitting

- [ ] Was this discussed/approved via a Github issue? (no need for typos, doc improvements)
- [x] Did you read the [contributor guideline](https://github.com/pytorch/fairseq/blob/main/CONTRIBUTING.md)?
- [ ] Did you make sure to update the docs?
- [ ] Did you write any new necessary tests?

## What does this PR do?
Fixes logits masking bug introduced in https://github.com/fairinternal/fairseq-py/issues/2469

## PR review
Anyone in the community is free to review the PR once the tests have passed.
If we didn't discuss your PR in Github issues there's a high chance it will not be merged.

## Did you have fun?
Make sure you had fun coding �

Pull Request resolved: https://github.com/fairinternal/fairseq-py/pull/2774

Reviewed By: arbabu123

Differential Revision: D33003148

Pulled By: alexeib

fbshipit-source-id: 19bf3da4f5104b33357fd4941e5e76b95174ee28

fairseq/models/wav2vec/wav2vec2_asr.py
@@ -195,8 +195,10 @@ class Wav2VecCtc(BaseFairseqModel):
 
         if net_output["padding_mask"] is not None and net_output["padding_mask"].any():
             number_of_classes = logits.size(-1)
-            masking_tensor = torch.ones(number_of_classes) * float("-inf")
-            masking_tensor[0] = float("inf")
+            masking_tensor = torch.ones(
+                number_of_classes, device=logits.device
+            ) * float("-inf")
+            masking_tensor[0] = 0
             logits[net_output["padding_mask"].T] = masking_tensor.type_as(logits)
 
         if normalize: